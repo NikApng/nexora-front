9d7217be69193f05df85b59d75d56c4bb131aaea
NiksoC <krasilnikovnikita11@gmail.com>
2025-11-26 00:14:57 +0300

    окно регистрации верстка

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,18 @@
+export default function RegisterPage() {
+    return (
+        <>
+            <main>
+                <div>
+                    <div>
+                        <h2>Регистрация</h2>
+                    </div>
+                    <div>
+                        <form action="">
+
+                        </form>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+}
