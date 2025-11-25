d64b3f951a887c1558c326e736ec251a247fa2ed
NiksoC <krasilnikovnikita11@gmail.com>
2025-11-26 00:37:38 +0300

    Верстка есть

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,18 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
 export default function RegisterPage() {
     return (
-        <>
-            <main>
-                <div>
-                    <div>
-                        <h2>Регистрация</h2>
-                    </div>
-                    <div>
-                        <form action="">
+        <main className={'flex flex-col justify-center items-center w-full h-full '}>
+        <Card className="w-[400px] p-6">
+            <CardHeader>
+                <h1 className="text-2xl font-bold text-center">Create account</h1>
+            </CardHeader>
 
-                        </form>
-                    </div>
+            <CardContent className="space-y-4">
+                <div className="space-y-2">
+                    <Label>Имя</Label>
+                    <Input />
                 </div>
-            </main>
-        </>
+
+                <div className="space-y-2">
+                    <Label>Почта</Label>
+                    <Input type="email" />
+                </div>
+
+                <div className="space-y-2">
+                    <Label>Пароль</Label>
+                    <Input type="password" />
+                </div>
+            </CardContent>
+
+            <CardFooter>
+                <Button className="w-full">Далее</Button>
+            </CardFooter>
+        </Card>
+        </main>
     );
 }
