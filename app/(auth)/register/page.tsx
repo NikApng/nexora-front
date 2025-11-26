05c31ea7199988e8aba75540578c4f36eee668c0
NiksoC <krasilnikovnikita11@gmail.com>
2025-11-26 19:24:54 +0300

    add last version

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,37 +1,46 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Card, CardHeader, CardContent, CardFooter} from "@/components/ui/card";
+import {Label} from "@/components/ui/label";
+import Link from "next/link";
+
+
 
 export default function RegisterPage() {
     return (
         <main className={'flex flex-col justify-center items-center w-full h-full '}>
-        <Card className="w-[400px] p-6">
-            <CardHeader>
-                <h1 className="text-2xl font-bold text-center">Create account</h1>
-            </CardHeader>
+            <Card className="w-[400px] p-6">
+                <CardHeader>
+                    <h1 className="text-2xl font-bold text-center">Create account</h1>
+                </CardHeader>
 
-            <CardContent className="space-y-4">
-                <div className="space-y-2">
-                    <Label>Имя</Label>
-                    <Input />
-                </div>
+                <CardContent className="space-y-4">
+                    <div className="space-y-2">
+                        <Label>Имя</Label>
+                        <Input/>
+                    </div>
 
-                <div className="space-y-2">
-                    <Label>Почта</Label>
-                    <Input type="email" />
-                </div>
+                    <div className="space-y-2">
+                        <Label>Почта</Label>
+                        <Input type="email"/>
+                    </div>
 
-                <div className="space-y-2">
-                    <Label>Пароль</Label>
-                    <Input type="password" />
-                </div>
-            </CardContent>
+                    <div className="space-y-2">
+                        <Label>Пароль</Label>
+                        <Input type="password"/>
+                    </div>
+                </CardContent>
 
-            <CardFooter>
-                <Button className="w-full">Далее</Button>
-            </CardFooter>
-        </Card>
+                <CardFooter className={'flex flex-col justify-center items-center w-full h-full '}>
+                    <Button className="">Далее</Button>
+                    <p className="text-sm text-center text-muted-foreground">
+                        Уже есть аккаунт?{" "}
+                        <Link href="/login" className="text-primary underline-offset-4 hover:underline">
+                            Войти
+                        </Link>
+                    </p>
+                </CardFooter>
+            </Card>
         </main>
     );
 }
